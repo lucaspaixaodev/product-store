3096d43f67d5eda471f33d78146b4e85f9ac0cfa
Lucas <lucas.gomes@timesharesolucoes.com.br>
2024-09-20 08:10:19 -0300

    feat: adciona componente no items para a lista

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -4,15 +4,14 @@ import { Product } from '../../shared/interfaces/product.interface';
 import { CardComponent } from './components/card/card.component';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
-import { MatDialog } from '@angular/material/dialog';
-import { DeleteDialogComponent } from '../delete-dialog/delete-dialog/delete-dialog.component';
 import { filter } from 'rxjs';
 import { DialogDeleteService } from '../../shared/services/dialog-delete.service';
+import { NoItemsComponent } from './components/no-items/no-items.component';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CardComponent, RouterLink, MatButtonModule],
+  imports: [CardComponent, RouterLink, MatButtonModule, NoItemsComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
@@ -21,7 +20,9 @@ export class ListComponent {
   private _router = inject(Router);
   private _dialogDeleteService = inject(DialogDeleteService);
 
-  public products$$ = signal<Product[]>(inject(ActivatedRoute).snapshot.data['products']);
+  public products$$ = signal<Product[]>(
+    inject(ActivatedRoute).snapshot.data['products']
+  );
 
   public onEdit(product: Product) {
     this._router.navigate(['/edit-product', product.id]);
